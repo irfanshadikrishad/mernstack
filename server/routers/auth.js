fb9d493db7860d0aeb68e1c1036ecc26a29e557d
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-07-15 01:54:17 +0600

    12

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -2,8 +2,23 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.route('/').get((req, res) => {
-    res.send('router');
+router.route('/login').post((req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400).json({ error: "Please fill the email and password" })
+    } else {
+        User.findOne({ email: email }).then(data => {
+            if (!data) {
+                res.status(400).json({ message: "Invalid Credentials" });
+            } else {
+                if (data.email == email && data.password == password) {
+                    res.status(200).send({ message: "Logged in successfull" })
+                } else {
+                    res.status(400).json({ message: "Invalid Credentials" });
+                }
+            }
+        })
+    }
 })
 
 router.post('/registration', (req, res) => {
