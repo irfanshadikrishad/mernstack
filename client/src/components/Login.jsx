d9f6ddecb78d6097cf7368782784e00614ffa988
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-07-20 19:53:13 +0600

    24

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 export default function Login() {
     return (
         <div className="container">
-            <h1>Login</h1>
+            <div className="reg__main">
+                <div className="log__left">
+                    <img className="reg__img" src="/favicon.png" alt="" />
+                    <p className="reg__login">
+                        Don't have an account?
+                        <NavLink to="/registration">Register</NavLink>
+                    </p>
+                </div>
+                <div className="log__right">
+                    <h1 className="nav__title">Login</h1>
+                    <form method="post" className="reg__form">
+                        <input name="email" type="text" placeholder="Email" autoComplete="off" />
+                        <input name="password" type="password" placeholder="Password" autoComplete="off" />
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
