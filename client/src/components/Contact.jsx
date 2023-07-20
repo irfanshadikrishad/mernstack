8857e60507ddea34be55b282f90882f9c00e74ac
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-07-20 20:44:19 +0600

    25

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,20 @@
 export default function Contact() {
     return (
         <div className="container">
-            <h1>Contact</h1>
+            <div className="contact__main">
+                <form method="post" className="contact__form">
+                    <h1 className="nav__title">Contact</h1>
+                    <div>
+                        <div className="contact__inputs">
+                            <input name="name" type="text" placeholder="Your Name" />
+                            <input name="email" type="email" placeholder="Your Email" />
+                            <input name="phone" type="number" placeholder="Your Phone" />
+                        </div>
+                        <textarea name="message"></textarea>
+                    </div>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
