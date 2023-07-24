5701505d883875ae52b74c5542b4161422a1a5c7
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-07-24 18:17:29 +0600

    39

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,40 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
+    const [username, setUserName] = useState('');
+    const callUser = async () => {
+        try {
+            const res = await fetch('http://localhost:3001/getdata',
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    credentials: "include"
+                });
+
+            const data = await res.json();
+
+            setUserName(data.name);
+
+            if (res.status !== 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
+
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    useEffect(() => {
+        callUser();
+    })
     return (
         <div className="container home">
-            <p className="welcome">Welcome to</p>
-            <h1 className="nav__title">MERN Stack</h1>
+            <p className="welcome">Welcome</p>
+            <h1 className="nav__title">{username == "" ? "MERN Stack Developer" : username}</h1>
+            <p className="welcome_message">{username == "" ? "" : "Happy, to see you back."}</p>
         </div>
     )
 }
